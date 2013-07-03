8cd6fdcf0bf9adc9c51a541c9cff9890987c7d52
Jason Cozens <jason.cozens@computer.org>
2013-07-03 12:32:43 +0100

    Start of Test framework.
    I want to be able to generate test input for the schedulers. This
    consists of implementing a Jobs generator.

---
+++ VisualStudio/EjC.Scheduling/Src/Spike01_Tests/JobsGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spike01_Tests
+{
+    public class JobsGenerator : IJobsGenerator
+    {
+        public List<DateTime> Jobs
+        {
+            get { return new List<DateTime>(); }
+        }
+    }
+}
---
+++ VisualStudio/EjC.Scheduling/Src/Spike01_Tests/JobsGeneratorTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Spike01_Tests
+{
+    /// <summary>
+    /// Summary description for JobsGeneratorTests
+    /// </summary>
+    [TestClass]
+    public class JobsGeneratorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Act
+            IJobsGenerator jobGenerator = new JobsGenerator();
+            //Assert
+            Assert.AreEqual(0, jobGenerator.Jobs.Count);
+        }
+    }
+}
